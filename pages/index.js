50934620b1919f26d8372082cc0b18e5341088b5
briancobbel <bcobbel@gmail.com>
2023-03-22 16:04:01 -0600

    add quotes

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,10 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <div class="quoteContainer">
+            <div id="quote"></div>
+            <div id="author"></div>
+          </div>
         </p>
       </main>
 
