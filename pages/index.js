166012d77d100ba1731149bc6e578e8a582ad18e
briancobbel <bcobbel@gmail.com>
2023-03-22 15:35:22 -0600

    add call

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,19 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7b0935bfcamshbac80d765728a94p11adb2jsnd711fa8e8f3c",
+    "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+  },
+};
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
 export default function Home() {
   return (
     <div className="container">
